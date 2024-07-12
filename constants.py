25ee4c4187d72e492c98f49bafdb413925e211f6
hannahker <hannah.ker96@gmail.com>
2024-07-12 16:57:29 -0700

    Update environment

--- constants.py
+++ constants.py
@@ -6,7 +6,9 @@ load_dotenv()
 
 BBOX_GLOBAL = [-180, -90, 180, 90]
 SAS_TOKEN_DEV = os.getenv("DSCI_AZ_SAS_DEV")
+SAS_TOKEN_PROD = os.getenv("DSCI_AZ_SAS_PROD")
 CONTAINER_GLOBAL = "global"
+CONTAINER_RASTER = "raster"
 STORAGE_ACCOUNT_DEV = "imb0chd0dev"
 STORAGE_ACCOUNT_PROD = "imb0chd0prod"
 
