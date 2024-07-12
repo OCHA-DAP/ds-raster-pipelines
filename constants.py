9878fbf1fcceb442687627a87e6a465bfa05ba89
Hannah Ker <hannah.ker96@gmail.com>
2024-07-12 11:17:12 -0700

    Mars seas5 grib download dbx (#1)
    * Add dummy notebook
    
    * Clean up code

--- constants.py
+++ constants.py
@@ -7,8 +7,6 @@ load_dotenv()
 
 BBOX_GLOBAL = [-180, -90, 180, 90]
 SAS_TOKEN_DEV = os.getenv("DSCI_AZ_SAS_DEV")
-# For running in Databricks
-SAS_TOKEN_DEV = dbutils.secrets.get(scope = "hannah-secrets", key = "DSCI_AZ_SAS_DEV")
 CONTAINER_GLOBAL = "global"
 STORAGE_ACCOUNT_DEV = "imb0chd0dev"
 STORAGE_ACCOUNT_PROD = "imb0chd0prod"
