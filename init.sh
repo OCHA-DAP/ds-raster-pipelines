a12c780c608128b4f3c1e68b3ef15923006eb3ac
hannahker <hannah.ker96@gmail.com>
2024-12-05 17:48:04 -0800

    try clean install

--- init.sh
+++ init.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-
 set -m
+
+# Remove all package lists
+sudo rm -rf /var/lib/apt/lists/*
+
+# Get fresh package lists and update
+sudo apt-get clean
 sudo apt-get update
-sudo apt-get install libeccodes-dev --assume-yes --fix-missing
+
+# Try installing with verbose output for better debugging
+sudo apt-get install -V libeccodes-dev --assume-yes --fix-missing
