b0560560b7cbd245b88bf65756fdc68a889dbca3
hannahker <hannah.ker96@gmail.com>
2024-12-05 18:04:14 -0800

    try more manual installs

--- init.sh
+++ init.sh
@@ -3,10 +3,14 @@ set -m
 
 # Remove all package lists
 sudo rm -rf /var/lib/apt/lists/*
-
-# Get fresh package lists and update
 sudo apt-get clean
 sudo apt-get update
 
-# Try installing with verbose output for better debugging
+# Install core dependencies first
+sudo apt-get install -V build-essential cmake libnetcdf-dev --assume-yes --fix-missing
+
+# Try jpeg dependency specifically since that was erroring
+sudo apt-get install -V libopenjpeg-dev --assume-yes --fix-missing
+
+# Then try eccodes
 sudo apt-get install -V libeccodes-dev --assume-yes --fix-missing
