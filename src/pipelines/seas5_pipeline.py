43da30030cb20962edc36cb4d314dc44caced1f7
hannahker <hannah.ker96@gmail.com>
2024-12-09 21:39:34 -0800

    enable backfill for seas5

--- src/pipelines/seas5_pipeline.py
+++ src/pipelines/seas5_pipeline.py
@@ -22,6 +22,7 @@ class SEAS5Pipeline(Pipeline):
             coverage=kwargs["coverage"],
             use_cache=kwargs["use_cache"],
         )
+        self.backfill = kwargs["backfill"]
         self.is_update = is_update
         self.start_year = start_year
         self.end_year = end_year
@@ -176,6 +177,8 @@ class SEAS5Pipeline(Pipeline):
         this_month = today.month
 
         self.logger.info(f"Running SEAS5 pipeline in {self.mode} mode...")
+
+        # Run for the latest available date
         if self.is_update:
             self.logger.info("Retrieving SEAS5 data from this month...")
             for fc_month in leadtime_utils.leadtime_months(this_month, 7):
@@ -185,6 +188,28 @@ class SEAS5Pipeline(Pipeline):
                 self.process_data(
                     raw_filename, cur_year, issued_month=this_month, fc_month=fc_month
                 )
+
+        if self.backfill:
+            self.logger.info("Checking for missing data and backfilling if needed...")
+            missing_dates, coverage_pct = self.check_coverage()
+            self.print_coverage_report()
+            if coverage_pct != 100:
+                for missing_date in missing_dates:
+                    self.logger.debug(f"Getting data for {missing_date}...")
+                    missing_month = missing_date.month
+                    missing_year = missing_date.year
+                    for fc_month in leadtime_utils.leadtime_months(missing_month, 7):
+                        raw_filename = self.get_raw_data(
+                            year=missing_year,
+                            issued_month=missing_month,
+                            fc_month=fc_month,
+                        )
+                        self.process_data(
+                            raw_filename,
+                            missing_year,
+                            issued_month=missing_month,
+                            fc_month=fc_month,
+                        )
 
         else:
             self.logger.info(
